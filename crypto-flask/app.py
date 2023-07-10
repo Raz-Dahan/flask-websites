5c72a17c008eb20bc59f70f9752688cc2348bf3d
Raz-Dahan <razdahan31@gmail.com>
2023-07-10 11:44:14 +0300

    Add Dockerfile

--- crypto-flask/app.py
+++ crypto-flask/app.py
@@ -19,7 +19,6 @@ def index():
     background_url = "background.png"
     logo_url = "logo.png"
 
-
     return render_template('index.html', bitcoin_price=bitcoin_price, ethereum_price=ethereum_price, litecoin_price=litecoin_price, background_url=background_url, logo_url=logo_url)
 
 if __name__ == '__main__':
